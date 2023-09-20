43cd1f74b68f3fee808acd0c0e368d1786ed58c3
Mosubmohammed <mosup.050@gmail.com>
2023-09-20 23:31:35 +0300

    p

---
+++ index.js/main.js
@@ -0,0 +1,53 @@
+// /*=============== SHOW MENU ===============*/
+const navMenu=document.getElementById('nav-menu')
+const navToggle=document.getElementById('nav-toggle')
+const navClose=document.getElementById('nav-close')
+
+// MENU SHOW
+// VALIDATE IF CONSTANT EXISTS
+if(navToggle){
+    navToggle.addEventListener('click',() =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+// MENU HIDDEN
+// VALIDATE IF CONSTANT EXISTS
+if(navClose){
+    navClose.addEventListener('click',() =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+// /*=============== REMOVE MENU MOBILE ===============*/
+const navLink=document.querySelectorAll('.nav__link')
+const linkAction=() =>{
+const navMenu=document.getElementById('nav-menu')
+// when we click on each nav__link we remove the show-menu
+navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n=>n.addEventListener('click',linkAction))
+
+
+// /*=============== SWIPER PROJECTS ===============*/
+
+
+// /*=============== SWIPER TESTIMONIAL ===============*/
+
+
+// /*=============== EMAIL JS ===============*/
+
+
+// /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+
+
+// /*=============== SHOW SCROLL UP ===============*/ 
+
+
+// /*=============== DARK LIGHT THEME ===============*/ 
+
+
+// /*=============== CHANGE BACKGROUND HEADER ===============*/
+
+
+// /*=============== SCROLL REVEAL ANIMATION ===============*/
+
