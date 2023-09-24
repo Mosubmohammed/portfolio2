511d137c892d313d67e5af0a491494e398ecd192
Mosubmohammed <mosup.050@gmail.com>
2023-09-24 17:51:54 +0300

    .

--- index.js/main.js
+++ index.js/main.js
@@ -62,9 +62,63 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
 
 // /*=============== EMAIL JS ===============*/
 
+const contactForm =document.getElementById('contact-form')
+,contactName =document.getElementById('contact-name')
+,contactEmail =document.getElementById('contact-email')
+,contactProject =document.getElementById('contact-project')
+,contactMessage =document.getElementById('contact-message')
+
+const sendEmail = (e)=>{
+  e.preventDefault();
+
+  // !check if the field has a value
+  if(contactName.value === '' || contactEmail.value === '' || contactProject.value === ''){
+// !Add and remove color
+contactMessage.classList.remove('color-blue');
+contactMessage.classList.add('color-red');
+
+// !show a message
+
+contactMessage.textContent="write all the input fields 📩"
+  }else{
+    // !serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_0888aco','template_2vo4fnh','#contact-form','pqEBhP050_ujikO4r')
+
+    .then(()=>{
+// !Show message and add color
+contactMessage.classList.add('color-blue')
+contactMessage.textContent='Message sent ✅'
+
+// !Remove message after five seconds
+
+setTimeout(()=>{
+  contactMessage.textContent=''
+},5000)
+    }, (erro)=>{
+      alert('OOPS! SOMETHING HAS FAILED...', error)
+    })
+// !To clear the input field
+
+contactName.value=''
+contactEmail.value=''
+contactProject.value=''
+  }
+}
+  contactForm.addEventListener('submit',sendEmail)
 
 // /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const section=document.querySelectorAll('section[id]')
 
+const scrollActive = ()=>{
+  const scrollY=window.pageY0ffset
+
+  section.forEach(current =>{
+    const sectionHeight=current.offsetHeight,
+    sectionTop=current.offsetTop -58,
+    secondsId=current.getAttribute('id')
+    ,sectionClass=document.querySelector('.nav__menu a[href*='+section)
+  })
+}
 
 // /*=============== SHOW SCROLL UP ===============*/ 
 
@@ -75,5 +129,4 @@ let swiperTestimonial = new Swiper(".testimonial__container", {
 // /*=============== CHANGE BACKGROUND HEADER ===============*/
 
 
-// /*=============== SCROLL REVEAL ANIMATION ===============*/
-
+// /*=============== SCROLL REVEAL ANIMATION
