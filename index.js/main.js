645a6beb6beab025216ed4258c7e347ba23bafd8
Mosubmohammed <mosup.050@gmail.com>
2023-09-25 16:19:09 +0300

    .

--- index.js/main.js
+++ index.js/main.js
@@ -138,9 +138,45 @@ const scrollUp=() =>{
 }
 window.addEventListener('scroll',scrollUp)
 // /*=============== DARK LIGHT THEME ===============*/ 
+const themeButton = document.getElementById('theme-button')
+const darkTheme='dark-theme'
+const iconTheme='ri-sun-line'
 
+// ! previously selected topic (if user selected)
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon=localStorage.getItem('selected-icon')
 
+// ! we obtain the current them that the interface has by validating the dark-theme class
+
+const getCurrentTheme=()=>document.body.classList.contains(darkTheme)?'dark':'light'
+const getCurrentIcon=()=>themeButton.classList.contains(iconTheme)?'ri-moon-line':'ri-sun-line'
+
+// ! we validate if the user previously chose a  topic
+if(selectedTheme){
+  // ! if the validation is fulfilled , we ask what the issue was to know if we activated or deactivated the dark
+  document.body.classList[selectedTheme === 'dark'?'add':'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'ri-moon-line'?'add':'remove'](iconTheme)
+}
+
+//! activate / deactivate the theme manually with the button 
+themeButton.addEventListener('click',()=>{
+  // ! add or remove the dark / icon theme 
+  document.body.classList.toggle(darkTheme)
+  themeButton.classList.toggle(iconTheme)
+
+  // ! we save the theme and the current icon that the user chose
+
+localStorage.setItem('selected-theme',getCurrentTheme())
+localStorage.setItem('selected-icon',getCurrentIcon())
+
+})
 // /*=============== CHANGE BACKGROUND HEADER ===============*/
 
+const scrollHeader=()=>{
+  const header =document.getElementById('header')
+  // ! when the scroll is greater than 50 viewport height,add the scroll-header class to the header tag
+  this.scrollY>=50?header.classList.add('bg-header') 
+                  : header.classList.remove('bg-header')
+}
 
 // /*=============== SCROLL REVEAL ANIMATION
